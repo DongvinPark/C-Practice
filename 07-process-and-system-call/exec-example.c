6d44dc937978a5ce0f9f356ff461c9db511a7870
DongvinPark <dongvin99@naver.com>
2024-05-14 10:14:34 +0900

    주석 추가

--- 07-process-and-system-call/exec-example.c
+++ 07-process-and-system-call/exec-example.c
@@ -3,6 +3,11 @@
 #include <errno.h>
 #include <string.h>
 
+/*
+리눅스 환경이라면, ifconfig 명령을 터미널에서 실행하고,
+맥, 윈도우 환경에서는 ipconfig 명령을 실행하는 것과 같다.
+*/
+
 int main(){
     if( execl("/sbin/ifconfig", "/sbin/ifconfig", NULL) == -1 ){
         if( execlp("ipconfig", "ipconfig", NULL) == -1 ){
